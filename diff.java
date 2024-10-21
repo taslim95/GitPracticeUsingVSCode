341f07b0ab71af8ae193ebab021271f70d58e5b6
Taslim tassu <taslim95>
2024-10-21 16:06:25 +0530

    3rd commit

---
+++ diff.java
@@ -0,0 +1,5 @@
+public class diff {
+    public public static void main(String[] args) {
+        System.out.println("here is the difference");
+    }
+}
---
+++ sum.java
@@ -0,0 +1,10 @@
+public class sum {
+    public static void main (String [] args){
+        int a=10;
+        int b=20; 
+        sum =(a+b);
+    
+        System.out.println(sum);
+        System.out.println("First Commit happened");
+    }
+}
